a763c6045c5491a0703848bda8254cb60b311638
Thhomas1 <thomasromero10@hotmail.com>
2023-06-06 00:33:28 -0300

    Rendering Explained
    (Server side and Client side)

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,7 +1,7 @@
 import Navbar from '@/components/navbar/Navbar'
 import Footer from '@/components/footer/Footer'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Inter, Roboto } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
