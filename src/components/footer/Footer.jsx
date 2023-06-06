deaeb231da886448d3a8af0f94b9b234bc68aab9
Thhomas1 <thomasromero10@hotmail.com>
2023-06-05 22:44:43 -0300

    Layouts and Components

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <div>©2023 Thomas Romero</div>
+      <div></div>
+    </div>
+  );
+};
+// 20:30
+export default Footer;
