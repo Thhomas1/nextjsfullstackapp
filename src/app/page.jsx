36cc379f6c4b21ad3148cec0b1135ad94d23f770
Thomas Romero <93294587+Thhomas1@users.noreply.github.com>
2023-06-09 15:13:00 -0300

    Image explained finished

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -10,4 +10,6 @@ export default function Home() {
   );
 
   // 47:20 Next.js Image
+  // Si uno quiere usar imagenes externas       <Image width={500} height={500} src="https://images.pexels.com/photos/12546141/pexels-photo-12546141.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" className={styles.img} />
+
 }
