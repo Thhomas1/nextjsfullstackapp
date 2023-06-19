cd239aaf8742db33dc3dc7e9ad92b97eff1bd2d5
Thhomas1 <thomasromero10@hotmail.com>
2023-06-19 02:28:39 -0300

    Portfolio

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 const Portfolio = () => {
-  return <div className={styles.container}>Portfolio</div>;
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.selectTitle}>Algunos ejemplos</h1>
+      <div className={styles.items}>
+        <Link href="/portfolio/illustrations" className={styles.items}>
+          <span className={styles.title}>Ejemplos</span>
+        </Link>
+        <Link href="/portfolio/websites" className={styles.items}>
+          <span className={styles.title}>Websites</span>
+        </Link>
+        <Link href="/portfolio/application" className={styles.items}>
+          <span className={styles.title}>Applications</span>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Portfolio;
