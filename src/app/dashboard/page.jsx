f6ce186c7410421d40404f13db4ebcb78dacb57b
Thhomas1 <thomasromero10@hotmail.com>
2023-06-27 03:00:57 -0300

    Update page.jsx

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import styles from "./page.module.css";
 
 const Dashboard = () => {
+  const [data, setData] = useState([]);
+  const [err, setErr] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      setIsLoading(true);
+      const res = await fetch("http://jsonplaceholder.typicode.com/posts", {
+        cache: "no-store",
+      });
+
+      if (!res.ok) {
+        setErr(true);
+      }
+      setIsLoading(false);
+    };
+  }, []);
+
   return <div className={styles.container}>Dashboard</div>;
 };
 
